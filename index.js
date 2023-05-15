be7bedcec62421b51554564acd670eb6eba4b995
caclark17 <cla20055@byui.edu>
2023-05-15 13:15:56 -0600

    started project

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.listen(process.env.port || port);
+console.log('Web server is listening at port ' + (process.env.port || port));
