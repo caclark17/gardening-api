fafe4bd209c8344fd2136008c0dac6de86f5a3a7
caclark17 <cla20055@byui.edu>
2023-06-03 10:40:36 -0600

    adding logout

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,11 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+// logout
+router.get('/logout', (req,res) => {
+    res.send('logging out');
+});
+
 // google auth
 router.get('/google', (req, res) => {
     res.send('login with google');
